dc1a73a25ab515688ab724afd28a47bfcc7a7969
Michal Havlicek <miky.havlicek@gmail.com>
2022-09-07 20:09:27 +0200

    Add script to more concisely generate bindings.

---
+++ gen-bindings.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/sh
+
+VULKAN_DOCS="https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/main"
+
+if ! test -f "vk.xml"; then
+    echo Downloading vk.xml
+    wget "$VULKAN_DOCS/xml/vk.xml" 2> /dev/null
+fi
+
+if ! test -f "video.xml"; then
+    echo Downloading video.xml
+    wget "$VULKAN_DOCS/xml/video.xml" 2> /dev/null
+fi
+
+if test -z "$1"; then
+    echo \
+'Missing bindings output path, example usage:
+  ./gen-bindings.sh ~/Dev/slag "VK_VERSION_1_0, @surface, VK_KHR_SWAPCHAIN"'
+    exit 1
+fi
+
+if test -z "$2"; then
+    echo \
+'Missing generated extensions / features selection, example usage:
+  ./gen-bindings.sh ~/Dev/slag "VK_VERSION_1_0, @surface, VK_KHR_SWAPCHAIN"'
+    exit 1
+fi
+
+if command -v mold &> /dev/null; then
+    mold -run cargo run --bin generate $(realpath vk.xml)  $(realpath video.xml) ./slag-template "$1" "$2"
+else 
+    cargo run --bin generate $(realpath vk.xml)  $(realpath video.xml) ./slag-template "$1" "$2"
+fi
