7f1bdb314291d6c7bff505928839b98ba84211bc
Michal Havlicek <mihavlic@protonmail.com>
2023-02-10 22:32:37 +0100

    Derive Error for vk::Result

--- pumice/src/lib.rs
+++ pumice/src/lib.rs
@@ -12,6 +12,7 @@ pub mod util;
 pub mod extensions;
 
 pub type VulkanResult<T> = Result<T, vk::Result>;
+
 #[inline]
 pub fn new_result<T>(value: T, result: vk::Result) -> VulkanResult<T> {
     if result.0.is_negative() {
@@ -20,9 +21,21 @@ pub fn new_result<T>(value: T, result: vk::Result) -> VulkanResult<T> {
         VulkanResult::Ok(value)
     }
 }
+
 impl From<vk::Result> for VulkanResult<()> {
     fn from(value: vk::Result) -> Self {
-        new_result((), value)
+        if value.0.is_negative() {
+            VulkanResult::Err(value)
+        } else {
+            VulkanResult::Ok(())
+        }
+    }
+}
+
+impl std::error::Error for vk::Result {}
+impl std::fmt::Display for vk::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 pub mod vk;
